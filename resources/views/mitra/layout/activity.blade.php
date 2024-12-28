c82cfe52ef653d6c1a2a471b6fec4d730a45f544
dindafarras <dindafarrasg@gmail.com>
2024-12-28 22:22:31 +0700

    modified:   resources/js/mitra-fix.js 	modified:   resources/views/mitra/layout/activity.blade.php

--- resources/views/mitra/layout/activity.blade.php
+++ resources/views/mitra/layout/activity.blade.php
@@ -86,7 +86,7 @@
             <form action="{{ route('mitra.delete-kegiatan-action', ['id' => $mitra->id_mitra, 'id_keg' => $kegiatan->id_kegiatan]) }}" method="POST" style="display:inline;">
               @csrf
               @method('DELETE')
-              <button type="submit">
+              <button type="submit" onclick="event.stopPropagation();">
                 <svg class="w-7 stroke-red-500" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path
                     d="m18 6-.8 12.013c-.071 1.052-.106 1.578-.333 1.977a2 2 0 0 1-.866.81c-.413.2-.94.2-1.995.2H9.994c-1.055 0-1.582 0-1.995-.2a2 2 0 0 1-.866-.81c-.227-.399-.262-.925-.332-1.977L6 6M4 6h16m-4 0-.27-.812c-.263-.787-.394-1.18-.637-1.471a2 2 0 0 0-.803-.578C13.938 3 13.524 3 12.694 3h-1.388c-.829 0-1.244 0-1.596.139a2 2 0 0 0-.803.578c-.243.29-.374.684-.636 1.471L8 6"
