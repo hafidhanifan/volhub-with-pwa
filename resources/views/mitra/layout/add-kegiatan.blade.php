23a1ed411dec99825178debc5308037f8b260a25
Hafid Hanifan <hafid.hanifan123@gmail.com>
2024-12-29 07:28:09 +0700

    new file:   resources/views/backup/add-kegiatan-kriteria.blade.php 	new file:   resources/views/backup/add-kegiatan.blade.php 	new file:   resources/views/backup/edit-kegiatan.blade.php 	modified:   resources/views/mitra/layout/add-kegiatan-benefit.blade.php 	modified:   resources/views/mitra/layout/add-kegiatan-kriteria.blade.php 	modified:   resources/views/mitra/layout/add-kegiatan.blade.php 	modified:   resources/views/mitra/layout/edit-kegiatan.blade.php

--- resources/views/mitra/layout/add-kegiatan.blade.php
+++ resources/views/mitra/layout/add-kegiatan.blade.php
@@ -1,136 +1,71 @@
 @include('mitra.layout.templates.header')
 @include('mitra.layout.templates.sidebar')
-@include('mitra.layout.templates.navbar')  
-<!-- ========== section start ========== -->
-  <section class="section">
-    <div class="container-fluid">
-      <!-- ========== title-wrapper start ========== -->
-      <div class="title-wrapper pt-30">
-            <div class="row align-items-center">
-              <div class="col-md-6">
-                <div class="title">
-                  <h2>Tambah Project Volunteer</h2>
-                </div>
-              </div>
-              <!-- end col -->
-              <div class="col-md-6">
-                <div class="breadcrumb-wrapper">
-                  <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                      <li class="breadcrumb-item">
-                        <a href="#0">Dashboard</a>
-                      </li>
-                      <li class="breadcrumb-item active" aria-current="page">
-                        Page
-                      </li>
-                    </ol>
-                  </nav>
-                </div>
-              </div>
-              <!-- end col -->
-            </div>
-            <!-- end row -->
-          </div>
-          <!-- ========== title-wrapper end ========== -->
-          <!-- ========== form-elements-wrapper start ========== -->
-          <div class="form-elements-wrapper">
-            
-            <div class="row">
-              <div class="col-lg-12">
-                <form action="{{ route('mitra.add-kegiatan-action', ['id' => $mitra->id_mitra]) }}" method="POST" enctype="multipart/form-data">
-                  @csrf
-                <!-- input style start -->
-                <div class="card-style mb-30 tambah-kegiatan">
-                  <div class="input-style-1">
-                    <label>Nama Kegiatan</label>
-                    <input type="text" name="nama_kegiatan"/>
-                  </div>
-                  <div class="input-style-1">
-                    <label>Nama Penyelenggara</label>
-                    <input type="text" name="nama_mitra" value="{{ $mitra->nama_mitra }}" readonly/>
-                  </div>
-                  <!-- end input -->
-                  <div class="select-style-1">
-                    <label>Kategori</label>
-                    <div class="select-position">
-                      <select name="kategori">
-                        <?php $no = 1 ?>
-                          <option value="">Pilih Kategori</option>
-                          @foreach($kategori as $kategori) 
-                            <option value="{{$kategori->id_kategori}}">{{ $kategori->nama_kategori }}</option>
-                          @endforeach
-                      </select>
-                    </div>
-                  </div>
-                  <!-- end input -->
-                  <div class="select-style-1">
-                    <label>Sistem Kegiatan</label>
-                    <div class="select-position">
-                      <select name="sistem_kegiatan">
-                        <option value="">Pilih Sistem Kerja</option>
-                        @foreach($sistemKegiatan as $sistem)
-                          <option value="{{ $sistem }}">{{ ucfirst($sistem) }}</option>
-                        @endforeach
-                      </select>
-                    </div>
-                  </div>
-                  <!-- end input -->
-                  <div class="input-style-1">
-                    <label>Tanggal Penutupan</label>
-                    <input type="date" name="tgl_penutupan"/>
-                  </div>
-                  <!-- end input -->
-                  <div class="input-style-1">
-                    <label>Tanggal Kegiatan</label>
-                    <input type="date" name="tgl_kegiatan"/>
-                  </div>
-                  <!-- end input -->
-                  <div class="input-style-1">
-                    <label>Lama Kegiatan</label>
-                    <input type="text" name="lama_kegiatan"/>
-                  </div>
-                  <!-- end input -->
-                  <div class="input-style-1">
-                    <label>Lokasi</label>
-                    <input type="text" name="lokasi_kegiatan"/>
-                  </div>
-                  <!-- end input -->
-                  <div class="input-style-1">
-                    <label>Upload Gambar</label>
-                    <input type="file" name="gambar"/>
-                  </div>
-                  <!-- end input -->
-                </div>
-                <!-- end card -->
-                <!-- ======= input style end ======= -->
-
-                <!-- ======= textarea style start ======= -->
-                <div class="card-style mb-30">
-                    <div class="input-style-1">
-                      <label>Deskripsi</label>
-                      <textarea rows="5" name="deskripsi"></textarea>
-                    </div>
-                    <!-- end textarea -->
-                </div>
-                <!-- ======= textarea style end ======= -->
-                
-              </div>
-              <!-- end col -->
-
-          <!-- end card -->
-          <!-- ======= input style end ======= -->
-
-              </div>
-              <!-- end col -->
-            </div>
-            <!-- end row -->
-            
-            <button type="submit" href="#0" class="main-btn-kategori primary-btn btn-hover right-align">Tambah Kegiatan</button>
-          </form>
-          </div>
-          <!-- ========== form-elements-wrapper end ========== -->
+@include('mitra.layout.templates.navbar')
+<!-- Content Start -->
+<section class="w-full">
+  <div class="py-8 px-12 max-w-5xl">
+    <h1 class="mb-4 text-xl font-normal">Add a new volunteer</h1>
+    <form action="#">
+      <div class="grid gap-4 mt-8 sm:grid-cols-2 sm:gap-6">
+        <div class="col-span-2">
+          <label for="name" class="block mb-2 text-sm font-medium">Volunteer Name</label>
+          <input type="text" name="" id=""
+            class="bg-gray-50 border border-gray-300 text-sm rounded-lg block w-full p-2.5"
+            placeholder="Type volunteer name" required="" />
         </div>
-        <!-- end container -->
-  </section>
-      <!-- ========== section end ========== -->
+        <div class="col-span-2">
+          <label for="" class="block mb-2 text-sm font-medium">Location</label>
+          <input type="text" name="" id=""
+            class="bg-gray-50 border border-gray-300 text-sm rounded-lg block w-full p-2.5"
+            placeholder="Volunteer location" required="" />
+        </div>
+        <div class="col-span-2 lg:col-span-1">
+          <label for="price" class="block mb-2 text-sm font-medium">Volunteer Duration</label>
+          <input type="number" name="price" id="price"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+            placeholder="Volunteer duration" required="" />
+        </div>
+        <div class="col-span-2 lg:col-span-1">
+          <label for="category" class="block mb-2 text-sm font-medium">Volunteer System</label>
+          <select id="category" class="bg-gray-50 border border-gray-300 text-sm rounded-lg block w-full p-2.5">
+            <option selected="">Select system</option>
+            <option value="">Offline</option>
+            <option value="">Online</option>
+          </select>
+        </div>
+        <div class="col-span-2 lg:col-span-1">
+          <label for="start_date" class="block mb-2 text-sm font-medium">
+            Volunteer start
+          </label>
+          <input type="date" name="start_date" id="start_date"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+            placeholder="Pilih tanggal kegiatan dimulai" required />
+        </div>
+
+        <div class="col-span-2 lg:col-span-1">
+          <label for="end_date" class="block mb-2 text-sm font-medium">
+            Registration closed
+          </label>
+          <input type="date" name="end_date" id="end_date"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+            placeholder="Pilih tanggal pendaftaran berakhir" required />
+        </div>
+
+        <div class="col-span-2">
+          <label for="description" class="block mb-2 text-sm font-medium">Description</label>
+          <textarea id="description" rows="8"
+            class="block p-2.5 w-full text-sm bg-gray-50 rounded-lg border border-gray-300"
+            placeholder="Your description here"></textarea>
+        </div>
+      </div>
+      <div class="flex justify-end">
+        <button type="submit"
+          class="px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center bg-sky-500 text-white rounded-lg hover:bg-sky-600">
+          Add volunteer
+        </button>
+      </div>
+    </form>
+  </div>
+</section>
+<!-- Content End -->
 @include('mitra.layout.templates.footer')
