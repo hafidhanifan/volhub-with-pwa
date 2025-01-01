5bbd8e729652a82d334fd118ebaad02757c29241
Hafid Hanifan <hafid.hanifan123@gmail.com>
2025-01-01 14:10:59 +0700

    modified:   resources/views/user/layout/templates/header.blade.php

--- resources/views/user/layout/templates/header.blade.php
+++ resources/views/user/layout/templates/header.blade.php
@@ -5,26 +5,9 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="dicoding:f1836yb034@dicoding.org" content="hafidhanifan33@gmail.com">
-  <title>VolunteerHub</title>
-
-  @vite(['resources/css/app.css','resources/js/user-fix.js',])
-
-
   <link rel="shortcut icon" href="{{ asset('/img/logo-putih.png') }}" type="image/x-icon" />
-
-  <!-- Font -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link
-    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-    rel="stylesheet">
-
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-    integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-  <link href="https://cdn.jsdelivr.net/npm/cropperjs@1.5.12/dist/cropper.min.css" rel="stylesheet">
-
+  <title>VolunteerHub</title>
+  @vite(['resources/css/app.css','resources/js/user-fix.js',])
 </head>
 
 <body>
