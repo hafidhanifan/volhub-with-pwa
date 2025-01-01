5deb8c133d0e0f6505430b2b067dbf825faad87f
Hafid Hanifan <hafid.hanifan123@gmail.com>
2025-01-01 15:04:42 +0700

    modified:   resources/views/user/layout/templates/header.blade.php

--- resources/views/user/layout/templates/header.blade.php
+++ resources/views/user/layout/templates/header.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="dicoding:f1836yb034@dicoding.org" content="hafidhanifan33@gmail.com">
-  <link rel="shortcut icon" href="{{ asset('/img/logo-putih.png') }}" type="image/x-icon" />
+  <link rel="shortcut icon" href="{{ asset('/img/volhub-logo/volhub-navigation-logo.png') }}" type="image/x-icon" />
   <title>VolunteerHub</title>
   @vite(['resources/css/app.css','resources/js/user-fix.js',])
 </head>
