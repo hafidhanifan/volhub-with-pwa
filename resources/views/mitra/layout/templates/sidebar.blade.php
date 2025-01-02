fd55f9b08271f3736dbe6fbb1fc1f013025ea98a
Hafid Hanifan <hafid.hanifan123@gmail.com>
2025-01-02 12:28:06 +0700

    modified:   resources/views/mitra/layout/profile.blade.php 	modified:   resources/views/mitra/layout/templates/sidebar.blade.php

--- resources/views/mitra/layout/templates/sidebar.blade.php
+++ resources/views/mitra/layout/templates/sidebar.blade.php
@@ -46,7 +46,8 @@
         </svg>Schedule</a>
     </li>
     <li class="py-4 hover:bg-button_hover2">
-      <a href="#" class="flex"><svg class="w-6 mr-4 ml-2" viewBox="0 0 100 100" xml:space="preserve">
+      <a href="{{ route('mitra.edit-profile-page', ['id' => $mitra->id_mitra]) }}" class="flex"><svg
+          class="w-6 mr-4 ml-2" viewBox="0 0 100 100" xml:space="preserve">
           <path
             d="M80 71.2V74c0 3.3-2.7 6-6 6H26c-3.3 0-6-2.7-6-6v-2.8c0-7.3 8.5-11.7 16.5-15.2.3-.1.5-.2.8-.4.6-.3 1.3-.3 1.9.1C42.4 57.8 46.1 59 50 59c3.9 0 7.6-1.2 10.8-3.2.6-.4 1.3-.4 1.9-.1.3.1.5.2.8.4 8 3.4 16.5 7.8 16.5 15.1z" />
           <ellipse cx="50" cy="36.5" rx="14.9" ry="16.5" />
